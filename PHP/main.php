8e7a321a5860269ac4f2100bd28bf14063749c7a
Anton Barinov <p.toxa@bk.ru>
2019-04-18 03:11:51 +0300

    Add files via upload

---
+++ PHP/main.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<link rel="shortcut icon" href="../images/logo.png" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="../CSS/head.css">
+<head>
+<meta charset = "UTF-8">
+	<title>Главная страница</title>
+</head>
+
+<body>
+
+<div class="auth">
+</div>
+
+<header>
+	<section class = "header">	
+		<img src="../img/logo.png" width="250" height="150">
+		<ul class = "head">
+			<li><a href="login.php">Вход</a></li>
+			<li><a href="signup.php">Разместить</a></li>
+		</ul>
+	</section>
+<form>
+  <input type="text" placeholder="Искать здесь...">
+  <button type="submit"></button>
+</form>
+</header>
+<div>
+	<div class = "grid">
+		<div class = "box">
+			<div class = "image">
+				<img scr = "">
+			</div>
+		<h3>Недвижимость</h3>
+		<ul>
+			<li>Жилая</li>
+			<li>Коммерческая</li>
+			<li>Загородная</li>
+			<li>Ещё</li>
+		</ul>
+		</div>
+
+		<div class = "box">
+			<div class = "image">
+				<img scr = "">
+			</div>
+		<h3>Транспорт</h3>
+		<ul>
+			<li>Автомобили</li>
+			<li>Мотоциклы</li>
+			<li>Скутеры</li>
+		</ul>
+		</div>
+
+		<div class = "box">
+			<div class = "image">
+				<img scr = "">
+			</div>
+		<h3>Электроника</h3>
+		<ul>
+			<li>Компьютеры</li>
+			<li>Ноутбуки</li>
+			<li>Планшеты</li>
+		</ul>
+		</div>
+
+		<div class = "box">
+			<div class = "image">
+				<img scr = "">
+			</div>
+		<h3>Животные</h3>
+		<ul>
+			<li>Кошки</li>
+			<li>Собаки</li>
+			<li>Рыбы</li>
+		</ul>
+	</div>
+</div>
+<!-- 
+<section class = "last"> -->
+	<div>
+			<img = "">
+				<div class = "grid">
+		<div class = "box">
+		<div class = "image">
+			<img scr = "">
+		</div>
+		<h3>Животные</h3>
+		<ul>
+			<li>Кошки</li>
+			<li>Собаки</li>
+			<li>Рыбы</li>
+		</ul>
+	</div>
+		<div class = "box">
+			<div class = "image">
+				<img scr = "">
+			</div>
+		<h3>Личные вещи</h3>
+		<ul>
+			<li>Женские</li>
+			<li>Мужские</li>
+			<li>Детские</li>
+		</ul>
+		</div>
+
+		<div class = "box">
+			<div class = "image">
+				<img scr = "">
+			</div>
+		<h3>Для дома</h3>
+		<ul>
+			<li>Мебель</li>
+			<li>Строительство</li>
+			<li>Интерьер</li>
+		</ul>
+		</div>
+
+<!-- </section> -->
+
+
+<footer class "footer__menu">
+	<div class="grid_1">
+		<div class = "custom">
+			<p>Справочный телефон : 484848</p>
+		</div>
+
+		<div>
+		<ul class="head head-up">
+			<li>Контакты</li>
+			<li>Ищут дом</li>
+			<li>Каталог товаров</li>
+		</ul>
+		</div>
+
+		<div>
+			<img src="../img/vkicon.png" class="footer__icon">
+			<img src="../img/okicon.png" class="footer__icon">
+			<img src="../img/twittericon.png" class="footer__icon">
+		</div>
+
+	</div>
+</footer>
+</body>
+
+</html>
